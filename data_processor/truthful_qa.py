1873e9012f55402fef0d2aef06109bc9a0afd6ce
Marco g2 <zhuangc19@gmail.com>
2024-11-24 22:28:33 -0500

    file path

--- data_processor/truthful_qa.py
+++ data_processor/truthful_qa.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def main():
-    df = pd.read_json("=data/truthful_qa/truthful_qa.json")
+    df = pd.read_json("data/truthful_qa/truthful_qa.json")
     df_question = df[["question"]]
     df_question.columns = ["question"]
     return df_question
